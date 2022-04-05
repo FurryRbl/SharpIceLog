a19b246cbdb3ebb122e6ca8dc3f8063da3a68d7e
FurryRbl <VupRbl@gmail.com>
2022-04-05 10:19:30 +0800

    修复错误，优化代码

--- SharpIceLog/Logger.cs
+++ SharpIceLog/Logger.cs
@@ -4,35 +4,36 @@ namespace SharpIceLog
 {
     public interface Logger
     {
+        public static string Time = DateTime.Now.ToString("hh:mm:ss");
         public static void LogInfo(string Logs)
         {
             string CallerName = new StackTrace().GetFrame(1).GetMethod().ReflectedType.FullName + "." + new StackTrace().GetFrame(1).GetMethod().Name.ToString(); // 获取调用者命名空间，类名和方法名
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("[" + DateTime.Now.ToString("hh:mm:ss") + "]" + " " + "[" + CallerName + "/INFO" + "]" + ": " + Logs);
+            Console.WriteLine("[" + Time + "]" + " " + "[" + CallerName + "/INFO" + "]" + ": " + Logs);
         }
         public static void LogWarning(string Logs)
         {
             string CallerName = new StackTrace().GetFrame(1).GetMethod().ReflectedType.FullName + "." + new StackTrace().GetFrame(1).GetMethod().Name.ToString(); // 获取调用者命名空间，类名和方法名
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("[" + DateTime.Now.ToString("hh:mm:ss") + "]" + " " + "[" + CallerName + "/Warning" + "]" + ": " + Logs);
+            Console.WriteLine("[" + Time + "]" + " " + "[" + CallerName + "/Warning" + "]" + ": " + Logs);
         }
         public static void LogError(string Logs)
         {
             string CallerName = new StackTrace().GetFrame(1).GetMethod().ReflectedType.FullName + "." + new StackTrace().GetFrame(1).GetMethod().Name.ToString(); // 获取调用者命名空间，类名和方法名
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("[" + DateTime.Now.ToString("hh:mm:ss") + "]" + " " + "[" + CallerName + "/Error" + "]" + ": " + Logs);
+            Console.WriteLine("[" + Time + "]" + " " + "[" + CallerName + "/Error" + "]" + ": " + Logs);
         }
         public static void LogFatal(string Logs)
         {
             string CallerName = new StackTrace().GetFrame(1).GetMethod().ReflectedType.FullName + "." + new StackTrace().GetFrame(1).GetMethod().Name.ToString(); // 获取调用者命名空间，类名和方法名
             Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.WriteLine("[" + DateTime.Now.ToString("hh:mm:ss") + "]" + " " + "[" + CallerName + "/Fatal" + "]" + ": " + Logs);
+            Console.WriteLine("[" + Time + "]" + " " + "[" + CallerName + "/Fatal" + "]" + ": " + Logs);
         }
         public static void LogDebug(string Logs)
         {
             string CallerName = new StackTrace().GetFrame(1).GetMethod().ReflectedType.FullName + "." + new StackTrace().GetFrame(1).GetMethod().Name.ToString(); // 获取调用者命名空间，类名和方法名
             Console.ForegroundColor = ConsoleColor.Gray;
-            Console.WriteLine("[" + DateTime.Now.ToString("hh:mm:ss") + "]" + " " + "[" + CallerName + "/Debug" + "]" + ": " + Logs);
+            Console.WriteLine("[" + Time + "]" + " " + "[" + CallerName + "/Debug" + "]" + ": " + Logs);
         }
     }
 }
